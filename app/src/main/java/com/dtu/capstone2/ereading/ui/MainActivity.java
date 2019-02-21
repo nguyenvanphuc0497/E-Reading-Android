0408e8a594bae741c6fe2ff05a7e2419a9bd197d
Nguyen Van Phuc <nguyenvanphuc0497@gmail.com>
2019-02-21 20:48:38 +0700

    change name package

--- app/src/main/java/com/dtu/capstone2/ereading/ui/MainActivity.java
+++ app/src/main/java/com/dtu/capstone2/ereading/ui/MainActivity.java
@@ -1,9 +1,9 @@
-package com.dtu.capstone2.ereadingandroid.ui;
+package com.dtu.capstone2.ereading.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.dtu.capstone2.ereadingandroid.R;
+import com.dtu.capstone2.ereading.R;
 
 public class MainActivity extends AppCompatActivity {
 
