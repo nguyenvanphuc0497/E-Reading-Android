e08399c97c14c00f80c56d9900b2cb84f0999bc4
thanhvan261297 <thanhvandb97@gmail.com>
2019-03-22 15:57:08 -0700

    layout va su kien cho entertext

--- app/src/main/java/com/dtu/capstone2/ereadingandroid/ui/MainActivity.java
+++ app/src/main/java/com/dtu/capstone2/ereadingandroid/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.EditText;
 
 import com.dtu.capstone2.ereadingandroid.R;
 import com.dtu.capstone2.ereadingandroid.datasource.repository.EReadingRepository;
@@ -15,6 +16,7 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
     private EReadingRepository localRepository = new EReadingRepository();
     private String tk = "";
 
@@ -23,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.enter_text);
+        editText = findViewById(R.id.edittext);
+        //editText.setText(); //thiết lập nội dung
+      // editText.getText(); //lấy nội dung
 
         localRepository.login(new AccountLoginRequest("admin", "admin123456"))
                 .subscribeOn(Schedulers.io())
