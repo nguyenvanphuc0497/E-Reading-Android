94673fb8fe2622ead79628b6a8923af605d76867
Nguyen Van Phuc <nguyenvanphuc0497@gmail.com>
2019-04-08 17:24:43 +0700

    update indicate viewpager list new feed

--- app/src/main/java/com/dtu/capstone2/ereading/ui/newfeed/displayanewfeed/DisplayNewFeedViewModel.java
+++ app/src/main/java/com/dtu/capstone2/ereading/ui/newfeed/displayanewfeed/DisplayNewFeedViewModel.java
@@ -3,14 +3,14 @@ package com.dtu.capstone2.ereading.ui.newfeed.displayanewfeed;
 /**
  * Create by Nguyen Van Phuc on 4/7/19
  */
-public class DisplayNewFeedViewModel {
+class DisplayNewFeedViewModel {
     private String urlNewFeed;
 
-    public String getUrlNewFeed() {
+    String getUrlNewFeed() {
         return urlNewFeed;
     }
 
-    public void setUrlNewFeed(String urlNewFeed) {
+    void setUrlNewFeed(String urlNewFeed) {
         this.urlNewFeed = urlNewFeed;
     }
 }
--- app/src/main/java/com/dtu/capstone2/ereading/ui/newfeed/listnewfeed/ListNewFeedViewModel.java
+++ app/src/main/java/com/dtu/capstone2/ereading/ui/newfeed/listnewfeed/ListNewFeedViewModel.java
@@ -14,6 +14,7 @@ class ListNewFeedViewModel {
 
     List<ItemListNewFeedPager> getListItemPagers() {
         mListNewFeedPagers.add(new ItemListNewFeedPager(new PageListNewFeedFragment(), "News", ""));
+        mListNewFeedPagers.add(new ItemListNewFeedPager(new PageListNewFeedFragment(), "News", ""));
         return mListNewFeedPagers;
     }
 }
