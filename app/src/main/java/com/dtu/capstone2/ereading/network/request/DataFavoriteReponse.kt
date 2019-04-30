be06f1aace82ca2f22e5219771ef130ea569e0cf
Vo doan <v.t.doan1908@gmail.com>
2019-04-30 08:16:30 +0700

    favorite

---
+++ app/src/main/java/com/dtu/capstone2/ereading/network/request/DataFavoriteReponse.kt
@@ -0,0 +1,5 @@
+package com.dtu.capstone2.ereading.network.request
+
+/**
+ * Create by Vo The Doan on 04/30/2019
+ */
---
+++ app/src/main/java/com/dtu/capstone2/ereading/ui/favorite/FavoriteFragment.java
@@ -0,0 +1,7 @@
+package com.dtu.capstone2.ereading.ui.favorite;
+
+/**
+ * Create by Vo The Doan on 04/30/2019
+ */
+public class FavoriteFragment {
+}
